aec3ff64859216a03532b855663ecae481e8fd24
Victor Vlasenko <victor.vlasenko@sysgears.com>
2018-02-21 11:32:47 +0200

    Add initial Spring Boot code

---
+++ src/main/java/backend/HelloController.java
@@ -0,0 +1,13 @@
+package backend;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello World!";
+    }
+}
