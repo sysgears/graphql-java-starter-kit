abf487123b96156d4f0a72294868364d91c3ba30
Victor Vlasenko <victor.vlasenko@sysgears.com>
2018-02-22 19:15:21 +0200

    Add google-java-format plugin

--- src/main/java/backend/HelloController.java
+++ src/main/java/backend/HelloController.java
@@ -1,13 +1,13 @@
 package backend;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "Hello World!";
-    }
+  @RequestMapping("/")
+  public String index() {
+    return "Hello World!";
+  }
 }
