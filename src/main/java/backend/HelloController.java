553b899a9c94d324c95a56c4ba5765f47705d9c8
Victor Vlasenko <victor.vlasenko@sysgears.com>
2018-02-25 21:24:40 +0200

    Add FindBugs, code reformatting and pre-commit hook

--- src/main/java/backend/HelloController.java
+++ src/main/java/backend/HelloController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-  @RequestMapping("/")
-  public String index() {
-    return "Hello World!";
-  }
+    @RequestMapping("/")
+    public String index() {
+        return "Hello World!";
+    }
 }
