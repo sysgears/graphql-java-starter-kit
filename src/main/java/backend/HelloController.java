ac4adbd4d024c62c9c75a3b544bec78cc323e7f3
Victor Vlasenko <victor.vlasenko@sysgears.com>
2018-02-26 15:10:03 +0200

    Add sample GraphQL query implementation

--- src/main/java/backend/HelloController.java
+++ src/main/java/backend/HelloController.java
@@ -1,13 +1,37 @@
 package backend;
 
+import graphql.ExecutionResult;
+import graphql.GraphQL;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.StaticDataFetcher;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static graphql.schema.idl.RuntimeWiring.newRuntimeWiring;
 
 @RestController
 public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Hello World!";
+        String schema = "type Query{hello: String}";
+
+        SchemaParser schemaParser = new SchemaParser();
+        TypeDefinitionRegistry typeDefinitionRegistry = schemaParser.parse(schema);
+
+        RuntimeWiring runtimeWiring = newRuntimeWiring()
+                .type("Query", builder -> builder.dataFetcher("hello", new StaticDataFetcher("world"))).build();
+
+        SchemaGenerator schemaGenerator = new SchemaGenerator();
+        GraphQLSchema graphQLSchema = schemaGenerator.makeExecutableSchema(typeDefinitionRegistry, runtimeWiring);
+
+        GraphQL build = GraphQL.newGraphQL(graphQLSchema).build();
+        ExecutionResult executionResult = build.execute("{hello}");
+
+        return executionResult.getData().toString();
     }
 }
